003e99058147e892871f59d08604d375f9b0bb80
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-10-08 01:09:12 +0530

    commit errors resolve

--- djangoseries/urls.py
+++ djangoseries/urls.py
@@ -20,9 +20,9 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.all_series, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/',views.contact, name='contact'),
-    path('series/', include('chai.urls') ),
+    path('series/', include('series.urls') ),
 
 ]
