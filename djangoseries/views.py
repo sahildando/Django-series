e725e957f0143486f18215b1d38524c9961acb1e
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-10-07 11:26:54 +0530

    views,static,templates,html,css updated

--- djangoseries/views.py
+++ djangoseries/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello, world. You are at django series Home page")
+    #return HttpResponse("Hello, world. You are at django series Home page")
+ return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello, world. You are at django series About page")
